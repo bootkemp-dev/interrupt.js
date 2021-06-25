9f09bb038632bdb3a84035de5d2e2e7c4d52be0c
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-06-25 13:27:19 +0200

    Move fs declaration to the abstract FileSystem

--- src/fs/interfaces.ts
+++ src/fs/interfaces.ts
@@ -1,7 +1,18 @@
 export type FileContent = string | null;
-export type Path = { absolute: boolean; path: string[] };
+export type Path = { absolute: boolean; dirs: string[] };
 
 export abstract class FileSystem {
+  protected fs: FsDirectory;
+  private currentDirName: string;
+
+  constructor() {
+    this.fs = {
+      name: '/',
+      children: []
+    };
+    this.currentDirName = '/';
+  }
+
   abstract read(path: string): FileContent;
 
   abstract save(path: string, content: FileContent): void;
@@ -16,16 +27,9 @@ export abstract class FileSystem {
       throw new Error(`Invalid path: ${path}`);
     }
 
-    const absolute = path.startsWith('/');
-
-    const splitPath = path.split('/');
-    if (absolute) {
-      splitPath.unshift('/');
-    }
-
     return {
-      absolute,
-      path: splitPath.filter(val => val !== '')
+      absolute: path.startsWith('/'),
+      dirs: path.split('/').filter(val => val !== '')
     };
   }
 }
--- src/fs/localStorage.ts
+++ src/fs/localStorage.ts
@@ -1,16 +1,6 @@
 import { FsDirectory, FileContent, FileSystem } from './interfaces';
 
 export class LocalStorageFileSystem extends FileSystem {
-  private fs: FsDirectory;
-
-  constructor() {
-    super();
-    this.fs = {
-      name: '/',
-      children: []
-    };
-  }
-
   read(path: string): FileContent {
     throw new Error('Method not implemented.');
   }
